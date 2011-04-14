6ce32909df6555521cb54232e89fac6035856079
user2 <user2@multiseat.(none)>
2011-04-14 19:48:14 +0400

    templet

--- app/app.py
+++ app/app.py
@@ -5,10 +5,12 @@ __version__ = '2011.0413'
 print 'ver:', __version__
 
 import sqlite3
-import web
+import web, uuid
 render = web.template.render('templates/')
+#render.cache = True
 
 urls = [
+    '(.*)/cdx/', 'cdx',
     '(.*)/cdx', 'cdx',
     '(.*)', 'cdx1',
 ]
@@ -19,26 +21,34 @@ class cdx1(object):
         con = sqlite3.connect('overhead.sqlite')
         cur = con.cursor()
         i = web.input()
+        rez = u'Привет, Гость'
         if i:
-            n = i.name
-            p = i.passw
-            sql = u"select * from auth where user=? and passw=?"
-            cur.execute(sql, (n, p))
-            r = cur.fetchall()
-            if r:
-                global rez
-                rez = r[0][1]
-                raise web.redirect('/cdx')
-            else:
-                rez = u'Нет таких'
+            try:
+                if i.name and i.passw:
+                    n = i.name
+                    p = i.passw
+                    sql = u"select * from auth where user=? and passw=?"
+                    cur.execute(sql, (n, p))
+                    r = cur.fetchall()
+                    if r:
+                        rez = r[0][1]
+                        uid = uuid.uuid4()
+                        sql = u"UPDATE auth SET sesion=? WHERE user=?"
+                        cur.execute(sql, (uid.hex, n))
+                        con.commit()
+                        raise web.redirect('/cdx?sid='+uid.hex)
+
+                    else:
+                        rez = u'Нет таких'
+            except:
+                rez = u'Привет, Гость'
         else:
             rez = u'Привет, Гость'
-
 
         s = u'''
 <html>
 <head>
-
+<title>Авторизация</title>
 </head>
 
 <body>
@@ -56,25 +66,25 @@ class cdx1(object):
 </form>
 </body>
 </html>
-        ''' % rez 
-        return s
-        
-class cdx(object):
-    def GET(self, name):
-        web.header('Content-type', 'text/html; charset=utf-8')
-        #raise web.redirect('/')
-        s = u'''
-<html>
-<head>
-
-</head>
-
-<body>
-<h1>Привет, %s!</h1>
-</body>
-</html>
         ''' % rez
         return s
+
+class cdx(object):
+    def GET(self, name):
+        data = web.input()
+        sid = data.get('sid', '')
+        con = sqlite3.connect('overhead.sqlite')
+        cur = con.cursor()
+        sql = u"select * from auth where sesion=?"
+        cur.execute(sql, (sid,))
+        r = cur.fetchall()
+        if r:
+            rez = r[0][1]
+        else:
+            raise web.redirect('/')
+        web.header('Content-type', 'text/html; charset=utf-8')
+        return render.index({'name': rez, 'uuid': sid})
+
 app = web.application(urls, globals())
 if __name__ == "__main__":
     app.run()
