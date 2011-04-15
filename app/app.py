67a4dc227c530657126fe0a9bec4d750b0580f53
user2 <user2@multiseat.(none)>
2011-04-15 18:06:18 +0400

    111

--- app/app.py
+++ app/app.py
@@ -10,9 +10,11 @@ render = web.template.render('templates/')
 #render.cache = True
 
 urls = [
+    '(.*)/exit', 'exit',
     '(.*)/cdx/', 'cdx',
     '(.*)/cdx', 'cdx',
     '(.*)', 'cdx1',
+
 ]
 
 class cdx1(object):
@@ -20,6 +22,14 @@ class cdx1(object):
         web.header('Content-type', 'text/html; charset=utf-8')
         con = sqlite3.connect('overhead.sqlite')
         cur = con.cursor()
+        sid = web.cookies().get('sid')
+        if sid:
+            sql = u"select * from auth where sesion=?"
+            cur.execute(sql, (sid,))
+            r = cur.fetchall()
+            if r:
+                raise web.redirect('/cdx')
+
         i = web.input()
         rez = u'Привет, Гость'
         if i:
@@ -36,7 +46,9 @@ class cdx1(object):
                         sql = u"UPDATE auth SET sesion=? WHERE user=?"
                         cur.execute(sql, (uid.hex, n))
                         con.commit()
-                        raise web.redirect('/cdx?sid='+uid.hex)
+                        web.setcookie('sid', uid.hex, 3600)
+                        #raise web.redirect('/cdx?sid='+uid.hex)
+                        raise web.redirect('/cdx')
 
                     else:
                         rez = u'Нет таких'
@@ -49,7 +61,8 @@ class cdx1(object):
 class cdx(object):
     def GET(self, name):
         i = web.input()
-        sid = i.get('sid', '')
+        #sid = i.get('sid', '')
+        sid = web.cookies().get('sid')
         con = sqlite3.connect('overhead.sqlite')
         cur = con.cursor()
         sql = u"select * from auth where sesion=?"
@@ -58,12 +71,23 @@ class cdx(object):
         if r:
             rez = r[0][1]
         else:
-            pass
-            #raise web.redirect('/')
+            #pass
+            raise web.redirect('/')
         web.header('Content-type', 'text/html; charset=utf-8')
         out = render.main({'name': rez, 'uuid': sid})
-        #out.uuid
+        #print '1121:', out.uuid
         return out
+class exit(object):
+    def GET(self, name):
+        sid = web.cookies().get('sid')
+        if sid:
+            con = sqlite3.connect('overhead.sqlite')
+            cur = con.cursor()
+            uid = uuid.uuid4()
+            sql = u"UPDATE auth SET sesion=? WHERE sesion=?"
+            cur.execute(sql, (uid.hex, sid))
+            con.commit()
+        raise web.redirect('/')
 
 
 app = web.application(urls, globals())
