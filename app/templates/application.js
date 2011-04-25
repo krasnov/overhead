a4c306e30873be03e3d9bbeede99d741939f6a3c
zibadi <zibadi@delli.(none)>
2011-04-26 00:23:18 +0400

    up

---
+++ app/templates/application.js
@@ -0,0 +1,26 @@
+/*
+ * jQuery File Upload Plugin JS Example 4.4.1
+ * https://github.com/blueimp/jQuery-File-Upload
+ *
+ * Copyright 2010, Sebastian Tschan
+ * https://blueimp.net
+ *
+ * Licensed under the MIT license:
+ * http://creativecommons.org/licenses/MIT/
+ */
+
+/*global $ */
+
+$(function () {
+    // Initialize jQuery File Upload (Extended User Interface Version):
+    $('#file_upload').fileUploadUIX();
+    
+    // Load existing files:
+    $.getJSON($('#file_upload').fileUploadUIX('option', 'url'), function (files) {
+        var fileUploadOptions = $('#file_upload').fileUploadUIX('option');
+        $.each(files, function (index, file) {
+            fileUploadOptions.buildDownloadRow(file, fileUploadOptions)
+                .appendTo(fileUploadOptions.downloadTable).fadeIn();
+        });
+    });
+});
